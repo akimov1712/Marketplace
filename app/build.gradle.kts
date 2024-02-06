48a98a99051fd825cfdbb2f846f86a894772accb
topbun <akimov1712@mail.ru>
2024-02-06 11:54:00 +0300

    Added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,27 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id ("com.google.devtools.ksp")
+    id ("kotlin-parcelize")
+    id ("androidx.navigation.safeargs.kotlin")
+    id ("com.google.dagger.hilt.android")
 }
 
 android {
     namespace = "com.fake.marketplace"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.fake.marketplace"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        ksp {
+            arg("room.schemaLocation", "$projectDir/schemas")
+        }
     }
 
     buildTypes {
@@ -27,17 +34,46 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_18
+        targetCompatibility = JavaVersion.VERSION_18
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "18"
+    }
+    buildFeatures{
+        viewBinding = true
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    val room_version = "2.6.1"
+    val hilt = "2.48.1"
+    val nav_version = "2.7.6"
+
+//    Hilt
+    implementation ("com.google.dagger:hilt-android:$hilt")
+    ksp ("com.google.dagger:hilt-compiler:$hilt")
+
+//    Navigation
+    implementation ("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation ("androidx.navigation:navigation-ui-ktx:$nav_version")
+
+//    JSON
+    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation ("com.google.code.gson:gson:2.10.1")
+    implementation ("org.jsoup:jsoup:1.16.2")
+
+//    Room
+    implementation("androidx.room:room-runtime:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+
+//    ViewModel
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+
+
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
