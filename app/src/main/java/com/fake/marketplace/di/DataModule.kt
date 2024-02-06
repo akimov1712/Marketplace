464079db1e8cf51f13f28f2213f624edb056d4fb
topbun <akimov1712@mail.ru>
2024-02-06 14:53:43 +0300

    add provide dao in dataModule

--- app/src/main/java/com/fake/marketplace/di/DataModule.kt
+++ app/src/main/java/com/fake/marketplace/di/DataModule.kt
@@ -1,11 +1,14 @@
 package com.fake.marketplace.di
 
+import android.app.Application
 import com.fake.marketplace.data.repositories.account.AccountRepositoryImpl
 import com.fake.marketplace.data.repositories.product.ProductRepositoryImpl
+import com.fake.marketplace.data.source.locale.database.AppDatabase
 import com.fake.marketplace.domain.repository.account.AccountRepository
 import com.fake.marketplace.domain.repository.product.ProductRepository
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
@@ -21,5 +24,19 @@ interface DataModule {
     @Binds
     @Singleton
     fun bindProductRepository(impl: ProductRepositoryImpl): ProductRepository
+
+    companion object{
+
+        @Provides
+        @Singleton
+        fun provideAccountDao(application: Application) =
+            AppDatabase.getInstance(application).accountDao()
+
+        @Provides
+        @Singleton
+        fun provideProductDao(application: Application) =
+            AppDatabase.getInstance(application).productDao()
+
+    }
 
 }
