386677e8e6a71f0385c290132c5d6306093ccc29
topbun <akimov1712@mail.ru>
2024-02-06 13:30:46 +0300

    create product repository

---
+++ app/src/main/java/com/fake/marketplace/domain/repository/ProductRepository.kt
@@ -0,0 +1,12 @@
+package com.fake.marketplace.domain.repository
+
+import com.fake.marketplace.domain.entities.ProductEntity
+import kotlinx.coroutines.flow.Flow
+
+interface ProductRepository {
+
+    suspend fun getProductList(): Flow<List<ProductEntity>>
+    suspend fun getFavoriteProductList(): Flow<List<ProductEntity>>
+    suspend fun getProductItem(id: Int): Flow<ProductEntity>
+
+}
