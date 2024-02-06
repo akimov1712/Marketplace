26cc74e15bee6020f8ae5e3338b30abb8d7f1c58
topbun <akimov1712@mail.ru>
2024-02-06 13:10:28 +0300

    project split on layers

--- app/src/main/java/com/fake/marketplace/presentation/SingleActivity.kt
+++ app/src/main/java/com/fake/marketplace/presentation/SingleActivity.kt
@@ -1,11 +1,12 @@
-package com.fake.marketplace
+package com.fake.marketplace.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.fake.marketplace.R
 
-class MainActivity : AppCompatActivity() {
+class SingleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_single)
     }
 }
