d8776bc0c09861fdd924f6a82fa70758657cfe4c
topbun <akimov1712@mail.ru>
2024-02-06 14:05:03 +0300

    create repositoryImpl

---
+++ app/src/main/java/com/fake/marketplace/data/repositories/account/AccountRepositoryImpl.kt
@@ -0,0 +1,10 @@
+package com.fake.marketplace.data.repositories.account
+
+import javax.inject.Inject
+
+class AccountRepositoryImpl @Inject constructor(
+
+) {
+
+
+}
---
+++ app/src/main/java/com/fake/marketplace/data/repositories/product/ProductRepositoryImpl.kt
@@ -0,0 +1,10 @@
+package com.fake.marketplace.data.repositories.product
+
+import javax.inject.Inject
+
+class ProductRepositoryImpl @Inject constructor(
+
+) {
+
+
+}
